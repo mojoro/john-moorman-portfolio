7e077a374942e8cfb5281d419defe6b6f7997f22
John Moorman <45500240+mojoro@users.noreply.github.com>
2026-03-14 23:05:21 +0100

    Polish up hackathon post

--- components/mdx-image.tsx
+++ components/mdx-image.tsx
@@ -8,15 +8,17 @@ export function MdxImage(props: React.ComponentProps<"img">) {
   const src = typeof props.src === "string" ? props.src : ""
 
   return (
-    <Image
-      src={src}
-      alt={props.alt ?? ""}
-      width={900}
-      height={500}
-      className="my-6 cursor-zoom-in rounded-lg"
-      quality={100}
-      sizes="(max-width: 768px) 100vw, 680px"
-      onClick={() => open(src, props.alt ?? "")}
-    />
+    <div style={props.style}>
+      <Image
+        src={src}
+        alt={props.alt ?? ""}
+        width={900}
+        height={500}
+        className="my-6 cursor-zoom-in rounded-lg"
+        quality={100}
+        sizes="(max-width: 768px) 100vw, 680px"
+        onClick={() => open(src, props.alt ?? "")}
+      />
+    </div>
   )
 }
